c98a8ff1ccb206935ece40a4b952857c5c45b160
Felipe Neves <matosfnm@gmail.com>
2021-05-25 20:50:32 -0300

    Domain Model - Entidades

---
+++ src/main/java/escola/Aluno.java
@@ -0,0 +1,9 @@
+package escola;
+
+public class Aluno {
+
+    private String cpf;//identificador unico. Representacao mais proxima possivel no mundo real, do modelo negocial.
+    private String nome;
+
+    private Email email;
+}
